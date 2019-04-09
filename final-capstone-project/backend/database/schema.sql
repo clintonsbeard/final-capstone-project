4005cb935fb13aafe15ee047476b6ddcbe66e7ce
Stephanie <stephanieplopez@gmail.com>
2019-04-09 10:33:16 -0400

    Saving schema and pulling frontend code

--- final-capstone-project/backend/database/schema.sql
+++ final-capstone-project/backend/database/schema.sql
@@ -20,34 +20,33 @@ CREATE TABLE app_user (
 
 CREATE TABLE student(
 
-  		student_id SERIAL PRIMARY KEY,
-        first_name VARCHAR(100) NOT NULL,
-        last_name VARCHAR(100) NOT NULL,
-        choice_1 INTEGER NOT NULL,
-        choice_2 INTEGER NOT NULL,
-        choice_3 INTEGER NOT NULL,
-        choice_4 INTEGER NOT NULL
+  student_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  choice_1 INTEGER NOT NULL,
+  choice_2 INTEGER NOT NULL,
+  choice_3 INTEGER NOT NULL,
+  choice_4 INTEGER NOT NULL
 );
 
 CREATE TABLE employer(
 
-        employer_id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        last_name VARCHAR(100) NOT NULL,
-        logo VARCHAR(100),
-        information TEXT NOT NULL,
-        number_of_teams INTEGER NOT NULL
-
+  employer_id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  logo VARCHAR(100),
+  information TEXT NOT NULL,
+  number_of_teams INTEGER NOT NULL
 );
 
 
 CREATE TABLE student_employer(
 
-        student_id INT NOT NULL,
-        employer_id INT NOT NULL,
+  student_id INT NOT NULL,
+  employer_id INT NOT NULL,
         
-        constraint fk_student_employer_student_id foreign key (student_id) references student(student_id),
-        constraint fk_student_employer_employer_id foreign key (employer_id) references employer(employer_id)      
+  constraint fk_student_employer_student_id foreign key (student_id) references student(student_id),
+  constraint fk_student_employer_employer_id foreign key (employer_id) references employer(employer_id)      
 );
 
 

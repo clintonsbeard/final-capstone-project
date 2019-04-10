fcf23c2a7e707f902c25b925be1095109c63d583
Stephanie <stephanieplopez@gmail.com>
2019-04-10 15:12:34 -0400

    Saving Home Page

--- final-capstone-project/frontend/src/views/Home.vue
+++ final-capstone-project/frontend/src/views/Home.vue
@@ -7,7 +7,6 @@
         <img src="../assets/img/matchmaking-infographic.png" class="img-fluid">
         <hr class="my-4">
         <p class="text-center">With growing need to fill your talent pipeline with qualified tech talent, we host Company Matchmaking events to introduce partner companies to graduating Java and .NET cohorts. Matchmaking gives your hiring managers an opportunity to meet and assess the fit of potential candidates in short, timed sessions over the course of two days.</p>
-        <a href="/employer-form" role="button" class="btn btn-primary btn-lg">Register For Matchmaking</a>
       </div>
     </div>
   </div>

8666530f4205d7661a141f4635c40d9971bbbc8a
clintonsbeard <clintonjbeard@gmail.com>
2019-04-13 17:32:17 -0400

    Commit

---
+++ final-capstone-project/backend/src/main/java/com/techelevator/StudentAll/model/JDBCStudentAllDAO.java
@@ -0,0 +1,11 @@
+package com.techelevator.StudentAll.model;
+
+public class JDBCStudentAllDAO implements StudentAllDAO {
+
+	@Override
+	public StudentAll insertAllChoicesIntoDatabase(StudentAll studentAll) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ final-capstone-project/backend/src/main/java/com/techelevator/StudentAll/model/StudentAllDAO.java
@@ -0,0 +1,7 @@
+package com.techelevator.StudentAll.model;
+
+public interface StudentAllDAO {
+
+	public StudentAll insertAllChoicesIntoDatabase(StudentAll studentAll);
+	
+}

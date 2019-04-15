cc1e95460e4584b7f72f8ab3d4fde14742f0a4eb
Stephanie <stephanieplopez@gmail.com>
2019-04-15 16:06:40 -0400

    Pulling Schedule Page

--- final-capstone-project/backend/database/data.sql
+++ final-capstone-project/backend/database/data.sql
@@ -8,38 +8,38 @@ VALUES ('admin', 'jy072QcO/8+CBk2Zdo2jVQ==', 'YOTgqH3wx3OfAUugPhLjwtWcsH/GQuo+pF
 INSERT INTO app_user (user_name, password, salt, role) 
 VALUES ('dvega', 'FjZDm+sndmsdEDwNtfr6NA==', 'kidcasB0te7i0jK0fmRIGHSm0mYhdLTaiGkEAiEvLp7dAEHWnuT8n/5bd2V/mqjstQ198iImm1xCmEFu+BHyOz1Mf7vm4LILcrr17y7Ws40Xyx4FOCt8jD03G+jEafpuVJnPiDmaZQXJEpEfekGOvhKGOCtBnT5uatjKEuVWuDA=', 'student');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('SPL Inc', 'Consultanting Firm', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('SPL Inc', 'Consulting Firm', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('BIM Inc', 'Sells Coding Advice', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('BIM Inc', 'Sells Coding Advice', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('CJB Beardsley', 'Taking Over the World with Vue', 2, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('CJB Beardsley', 'Taking Over the World with Vue', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 2, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('AEP Electric Company', 'Gas', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('AEP Electric Company', 'Gas', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('Stepdad Corporation', 'Sorry', 1, 'Lactose Intolerant');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('Stepdad Corporation', 'Sorry', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'Lactose Intolerant');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('AEP Electric Company', 'Gas', 1, 'NONE');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('AEP Electric Company', 'Gas', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'NONE');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('Potatoes', 'We got em', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('Potatoes', 'We got em', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('Omni Consumer Products', 'Yeah, the company from Robocop', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('Omni Consumer Products', 'Yeah, the company from Robocop', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('Company', 'Product', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('Company', 'Product', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('Tech Elevator', 'Not an elevator repair school', 1, 'None');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('Tech Elevator', 'Not an elevator repair school', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 1, 'None');
 
-INSERT INTO employer (company_name, company_summary, number_of_teams, restrictions) 
-VALUES ('The Picture of Dorian Gray', 'Don''t do it', 3, 'Yes');
+INSERT INTO employer (company_name, company_summary, email, website, positions_summary, path_preference, number_of_teams, restrictions)
+VALUES ('The Picture of Dorian Gray', 'Don''t do it', 'test@test.com', 'http://www.test.com', 'We got jobz', 'Java', 3, 'Yes');
 
 INSERT INTO student (first_name, last_name) 
 VALUES ('Lincoln', 'Guthrie');
@@ -254,10 +254,10 @@ INSERT INTO employer_schedule (employer_id, schedule_id)
 VALUES (7, 1);
 
 INSERT INTO employer_schedule (employer_id, schedule_id) 
-VALUES (8, 1);
+VALUES (8, 2);
 
 INSERT INTO employer_schedule (employer_id, schedule_id) 
-VALUES (9, 1);
+VALUES (9, 2);
 
 INSERT INTO employer_schedule (employer_id, schedule_id) 
 VALUES (10, 1);

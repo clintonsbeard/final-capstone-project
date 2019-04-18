aa77507bfc5557afb970b59e132c7e53b802a92e
Bindiya <bindiya.sameem@gmail.com>
2019-04-18 11:01:19 -0400

    commit

--- final-capstone-project/frontend/src/views/ViewSchedule.vue
+++ final-capstone-project/frontend/src/views/ViewSchedule.vue
@@ -1,143 +1,25 @@
 <template>
     <div class="view-schedule">
-        <div class="container-fluid">
-            <div class="jumbotron">
-                <h1>View Final Schedule</h1>
-                <hr class="my-4">
-                <table class="table table-responsive table-borderless table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col" style="width: 5%"></th>
-                        <th scope="col" v-for="employer in employers" :key="employer.employerId" class="text-center align-middle" style="width: 5%"><input type="hidden" v-model="finalSchedule[employer.employerId]">{{employer.companyName}}</th>
-                    </tr>
-                </thead>
-                    <tbody>
-                        <tr v-for="(time) in timeArray" class="table-warning" v-if="time[0] === schedule.breakStartTime">
-                            <th scope="row" class="text-center align-middle">{{ [ time[0], "HH:mm" ] | moment("h:mm A") }} {{ time[1] }} {{ [ time[2], "HH:mm" ] | moment("h:mm A") }}</th>
-                            <td :colspan="employers.length" class="text-center align-middle">
-                                BREAK
-                            </td>
-                        </tr>
-                        <tr v-else>
-                            <th scope="row" class="text-center align-middle" style="width: 5%">{{ [ time[0], "HH:mm" ] | moment("h:mm A") }} {{ time[1] }} {{ [ time[2], "HH:mm" ] | moment("h:mm A") }}</th>
-                            <td v-for="(employer) in employers" :key="employer.employerId" class="text-center align-middle" style="width: 5%">
-                                    <select class="form-control" v-model="finalSchedule['Key' + employer.employerId + time[0].replace(':','')]"> 
-                                    <option value="" selected disabled>Choose...</option>
-                                    <option v-for="student in getStudents" :key="student.studentId + employer.employerId + time[0]" :value="{startTime: time[0], endTime: time[2], studentId: student.studentId, employerId: employer.employerId}">{{student.firstName}} {{student.lastName}}</option>
-                                </select>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+
     </div>
 </template>
 
 <script>
 export default {
-    props: [
-        'scheduleChoice'
-    ],
+
     data() {
         return{
-            employers: [],
-            everything: [],
-            studentsBySchedule: [],
-            students: [],
-            timeSlots: [],
-            schedule: [],
-            finalSchedule: {
-	            scheduleId: this.scheduleChoice,
-                studentNames: []
-            }
+            schedule: []
         }
     },
     created() {
-        fetch(`${process.env.VUE_APP_API_URL}/timeslots/${this.$route.params.scheduleChoice}`)
-        .then(response => {
+        fetch(`${process.env.VUE_APP_API_URL}/getFinalSchedule/${this.$route.params.scheduleChoice}`, {
+        }).then( (response) => {
             return response.json();
-        }).then ((timeSlots) => {
-            this.timeSlots = timeSlots;
-        }).catch(err => {
-            console.log(err);
-        });
-        fetch(`${process.env.VUE_APP_API_URL}/studentsBySchedule/${this.$route.params.scheduleChoice}`)
-        .then(response => {
-            return response.json();
-        }).then ((studentsBySchedule) => {
-            this.studentsBySchedule = studentsBySchedule;
-        }).catch(err => {
-            console.log(err);
-        });
-        fetch(`${process.env.VUE_APP_API_URL}/employersBySchedule/${this.$route.params.scheduleChoice}`)
-        .then(response => {
-            return response.json();
-        }).then ((employers) => {
-            this.employers = employers;
-        }).catch(err => {
-            console.log(err);
-        });
-        fetch(`${process.env.VUE_APP_API_URL}/schedule/${this.$route.params.scheduleChoice}`)
-        .then(response => {
-            return response.json();
-        }).then ((schedule) => {
-            this.schedule = schedule;   
-        }).catch(err => {
-            console.log(err);
-        });
-        fetch(`${process.env.VUE_APP_API_URL}/students`)
-        .then(response => {
-            return response.json();
-        }).then ((students) => {
-            this.students = students;
-        }).catch(err => {
-            console.log(err);
+        }).then ( (data) => {
+            console.table(data);
+            this.schedule = data;
         })
-    },
-    methods: {
-        submitFinalSchedule() {
-            //console.table(JSON.stringify(this.finalSchedule))
-            //console.log(Object.keys(this.finalSchedule))
-            const sendArray = [];
-            //sendArray.push("ScheduleId:" + this.finalSchedule.scheduleId);
-            Object.keys(this.finalSchedule).forEach(k => {
-                if(k.startsWith("Key")){
-                    sendArray.push(this.finalSchedule[k])
-                }
-            })
-            console.log(JSON.stringify(sendArray));
-
-            fetch(`${process.env.VUE_APP_API_URL}/submitFinalSchedule`, {
-                method: 'POST',
-                headers: {
-                    "Content-Type" : "application/json"
-                },
-                body: JSON.stringify(this.sendArray),
-            })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            .catch((err) => console.error(err));
-        }
-    },
-    computed: {
-        timeArray() {
-            let timeSlots = [this.timeSlots]
-            let timeArray = []
-        timeSlots.forEach(timeSlot => {
-            timeSlot.forEach(slot => {
-                slot = String(slot)
-                timeArray.push(slot.split(" "))
-            })
-        })
-        return timeArray;
-        },
-        getStudents() {
-            return [...this.students];
-        }
     }
 }
 </script>
